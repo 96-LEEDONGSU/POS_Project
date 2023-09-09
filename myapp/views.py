44ed483e6aa1ed774e2e03019d750c35aab5af8e
DESKTOP-A54MEP4\LEEDONGSU <lds961006@gamil.com>
2023-09-10 03:11:54 +0900

    230910 상품정보 페이지를 분할하고, 페이지 내에서 검색기능을 통하여 검색된 부분을 스크롤 스파이 되는 기능을 추가해야한다.

--- myapp/views.py
+++ myapp/views.py
@@ -7,7 +7,6 @@ import pymysql
 def index(request):
     return render(request, 'login_page.html')
 
-
 def loginsuccess(request):
     login_info = [request.POST['username'], request.POST['password']]
     context = {'data' : select_all_data()}
@@ -15,16 +14,30 @@ def loginsuccess(request):
         return render(request, 'main_page.html', context)
     else:
         return render(request, 'login_page.html')
+    
+def loginsuccess_test(request):
+    login_info = [request.POST['username'], request.POST['password']]
+    context = {'data' : select_all_data()}
+    if login_info[0] == 'lds961006' and login_info[1] == '635d0b4108':
+        return render(request, 'main_page.html', context)
+    else:
+        return render(request, 'login_page.html')
+
 
 def select_all_data():
     conn = pymysql.connect(host='localhost', user='root', password='1234', db='mydb', charset='utf8')
     cur = conn.cursor()
-    cur.execute("SELECT c_name, c_phone, c_address, c_memo from customer")
+    cur.execute("SELECT c_idx, c_name, c_phone, c_address, c_memo from customer")
     result = cur.fetchall()
     return_data = []
 
     for idx, value in enumerate(result):
-        return_data.append(value)
+        row = {'c_idx' : value[0],
+                       'c_name' : value[1],
+                       'c_phone' : value[2],
+                       'c_address' : value[3],
+                       'c_memo' : value[4]}
+        return_data.append(row)
 
     conn.close()
 
